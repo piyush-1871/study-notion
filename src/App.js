8263377c5ec52e5c50cb96e7957d043ac1912260
piyush-1871 <piyush1871b@gmail.com>
2023-04-15 00:53:22 +0530

    added private route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import './App.css';
-import Navbar from './components/Navbar.js'
-import Home from './pages/Home.js'
-import Login from './pages/Login.js'
-import Dashboard from './pages/Dashboard'
-import Signup from './pages/Signup'
-import {Routes, Route} from 'react-router-dom'
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import Home from "./pages/Home.js";
+import Login from "./pages/Login.js";
+import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup";
+import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div className="w-screen h-screen bg-black flex flex-col overflow-x-hidden">
-    
       <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn}/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn}/>} />
-      
-      
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/login"
+          element={<Login setIsLoggedIn={setIsLoggedIn} />}
+        />
+        <Route
+          path="/signup"
+          element={<Signup setIsLoggedIn={setIsLoggedIn} />}
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute isLoggedIn={isLoggedIn}>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </div>
   );
